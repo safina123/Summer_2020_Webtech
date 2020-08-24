c1a78ee796431052a29af3f5355a50d2f487e301
safina123 <68175717+safina123@users.noreply.github.com>
2020-08-25 00:28:42 +0600

    degree php

---
+++ FINAL_LAB_TASK_2/degree/degree.php
@@ -0,0 +1,20 @@
+<?php
+if(isset($_POST['submit']))
+{
+	if (isset($_POST['degree1'])) 
+	{
+		# code... 
+		echo $_POST['degree1'];
+	}
+	if (isset($_POST['degree2'])) 
+	{
+		# code... 
+		echo $_POST['degree2'];
+	}
+	if (isset($_POST['degree3'])) 
+	{
+		# code... 
+		echo $_POST['degree3'];
+	}
+}
+?>
