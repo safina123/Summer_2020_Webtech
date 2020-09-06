a5e7b2e56c6893b1b6fc9866898a7c0e107e09f8
safina123 <68175717+safina123@users.noreply.github.com>
2020-09-06 09:32:06 +0600

    update view

---
+++ Final_Lab_Exam/views/authorhome.php
@@ -0,0 +1,13 @@
+<?php
+	require_once('../php/session_header.php');
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Author Home</title>
+</head>
+<body>
+	<a href="../php/logout.php">Logout</a> 
+</body>
+</html>
